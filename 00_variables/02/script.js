f840b528205a755f7ef4bd0c09038f49ec90a4fd
stevechanemougam <81678890+stevechanemougam@users.noreply.github.com>
2021-03-31 13:28:24 +0400

    Update script.js

--- 00_variables/02/script.js
+++ 00_variables/02/script.js
@@ -1,4 +1,15 @@
 
-let a = 12
-let b = "22"
-let c = "42.21"
+let a = 12;
+let b = "22";
+let c = "42.21";
+
+a=toString(a);
+b=parseInt(b);
+c=parseFloat(c);
+
+console.log(a);
+console.log(typeof(a));
+console.log(b);
+console.log(typeof(b));
+console.log(c);
+console.log(typeof(c));
