820fa8a0fb1997c02704e2ab8a4f987a809822cc
stevechanemougam <81678890+stevechanemougam@users.noreply.github.com>
2021-03-31 14:23:01 +0400

    Update script.js

--- 00_variables/05/script.js
+++ 00_variables/05/script.js
@@ -1,2 +1,19 @@
 
 // Opérateur d’affectation
+
+let a=7;
+a+=2;
+let b=7;
+b-=2;
+let c=7;
+c*=2;
+let d=7;
+d/=2;
+let e=7;
+e%=2;
+
+console.log("7+=2 = " + a);
+console.log("7-=2 = " + b);
+console.log("7*=2 = " + c);
+console.log("7/=2 = " + d);
+console.log("7%=2 = " + e);
