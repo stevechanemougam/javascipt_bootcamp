3ec487e96332faff049bf27594dc56c775f364d5
stevechanemougam <81678890+stevechanemougam@users.noreply.github.com>
2021-03-31 13:44:35 +0400

    Update script.js

--- 00_variables/04/script.js
+++ 00_variables/04/script.js
@@ -1,2 +1,15 @@
 
 // opérateur arithmétiques
+const a = 7+2;
+const b = 7-2;
+const c = 7*2;
+const d = 7/2;
+const e = 7%2;
+const f = 7**2;
+
+console.log("7+2 = " + a);
+console.log("7-2 = " + b);
+console.log("7*2 = " + c);
+console.log("7/2 = " + d);
+console.log("7%2 = " + e);
+console.log("7**2 = " + f);
