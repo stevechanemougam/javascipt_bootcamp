4433b15cf61181b83cb4b2d03bb24d68be2a804c
stevechanemougam <81678890+stevechanemougam@users.noreply.github.com>
2021-03-31 17:01:20 +0400

    Update script.js

--- 01_conditions/02/script.js
+++ 01_conditions/02/script.js
@@ -7,22 +7,22 @@ let c = 22
 let d = 3
 let f = "3"
 
-if () {
+if (a<=0 && b<0) {
   console.log("1) a est inferieur OU egal à 0 et b est negatif")
 }
 
-if () {
+if (b<0 && c<0) {
   console.log("2) b est negatif ET c est negatif")
 }
 
-if () {
+if (c>=0 && d===3 && b<0 ) {
   console.log("3) c est positif ET d est egal a nombre 3 ET b est negatif")
 }
 
-if () {
+if (f===3 || d ===3 && b<0) {
   console.log("4) f est egal au nombre 3 OU d est egal au nombre 3 ET b est negatif")
 }
 
-if () {
+if (f!==3 && (d!==3 || b>=0)) {
   console.log("5) L'inverse de (4)")
 }
