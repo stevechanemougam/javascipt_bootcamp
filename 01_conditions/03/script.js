6d260fc9371269a5f0fde3c3266749d5f639bbb7
stevechanemougam <81678890+stevechanemougam@users.noreply.github.com>
2021-03-31 17:07:57 +0400

    Update script.js

--- 01_conditions/03/script.js
+++ 01_conditions/03/script.js
@@ -1,8 +1,12 @@
 
 // operateur logique
 
-let a = Math.floor(Math.random() * Math.floor(3))
-
+let a = Math.floor(Math.random() * Math.floor(3));
+console.log(a);
 switch (a) {
-
+case 1: console.log("Le résultat est 1");
+        break;
+case 2: console.log("Le résultat est 2");
+        break;
+default: console.log("Le résultat est 0");
 }
