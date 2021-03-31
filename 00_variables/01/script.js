c0f97fbf52cd55b1d66d9008402860fabe5b912b
stevechanemougam <81678890+stevechanemougam@users.noreply.github.com>
2021-03-31 13:22:08 +0400

    Update script.js

---
+++ 00_variables/01/script.js
@@ -0,0 +1,9 @@
+const entier = 1;
+const flottant = 1.2;
+const booleen = true;
+const chaine = "test";
+
+console.log (typeof(entier));
+console.log (typeof(flottant));
+console.log (typeof(booleen));
+console.log (typeof(chaine));
