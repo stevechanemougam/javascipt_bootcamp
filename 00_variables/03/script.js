02f8aabaaeaecf13ae828fad5eb81e6a170fa4ee
stevechanemougam <81678890+stevechanemougam@users.noreply.github.com>
2021-03-31 13:37:08 +0400

    Update script.js

--- 00_variables/03/script.js
+++ 00_variables/03/script.js
@@ -1,8 +1,8 @@
-
 // concatenation
 
-let a = "Bonjour"
-let b = "je code"
-let c = "en"
-let d = "javascript"
-
+let a = "Bonjour";
+let b = "je code";
+let c = "en";
+let d = "javascript";
+const phrase = a + " " + b + " " + c + " " + d;
+console.log(phrase);
