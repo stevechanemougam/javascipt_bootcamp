3aaa66733d835e1fae655d486b13ec8c1eb8526f
stevechanemougam <81678890+stevechanemougam@users.noreply.github.com>
2021-04-01 16:53:54 +0400

    Update script.js

---
+++ 04_objets/03/script.js
@@ -0,0 +1,10 @@
+let personne = {
+    nom : "CHANEMOUGAM",
+    prenom : "Steve",
+    age : 37
+};
+
+
+personne = Object.assign({e_mail : "steve.chanemougam@gmail.com"}, personne);
+console.log(Object.keys(personne));
+console.log(Object.values(personne));
