155713c352e61785f5cfb25893b226e2179ff273
stevechanemougam <81678890+stevechanemougam@users.noreply.github.com>
2021-04-01 16:44:10 +0400

    Update script.js

---
+++ 04_objets/00/script.js
@@ -0,0 +1,9 @@
+let personne = {
+    nom : "CHANEMOUGAM",
+    prenom : "Steve",
+    age : 37
+};
+
+console.log(personne.nom);
+console.log(Object.keys(personne));
+console.log(Object.values(personne));
